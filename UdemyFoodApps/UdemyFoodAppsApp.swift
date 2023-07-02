640f5631b55fbca8a2ac7b0bce283af650a13aeb
hhakangull <hakanngul@icloud.com>
2023-07-02 01:03:24 +0100

    Initial Commit

---
+++ UdemyFoodApps/UdemyFoodAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdemyFoodAppsApp.swift
+//  UdemyFoodApps
+//
+//  Created by Hakan Gül on 02/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct UdemyFoodAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
